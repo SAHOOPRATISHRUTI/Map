f9c84b3b56d1cf306937b75cc701c588b24ae61a
Pratishruti Sahoo <pratishrutintspl@gmail.com>
2025-11-27 17:08:33 +0530

    commit by Pratishruti at 17.08

--- src/components/data/odishaData.js
+++ src/components/data/odishaData.js
@@ -9,59 +9,166 @@ const odishaData = {
   formationDay: "1st April 1936",
   oldNames: "Udra, Utkal, Kalinga, Kosala",
   capital: "Bhubaneswar",
-
+  Blocks: "314",
+  Tahasil: "317",
   categories: [
     {
       title: "General Facts",
       color: "#dffbff", // aqua
       items: [
+        //  { label: "Blocks", value: "314" },
+        //   { label: "Tahasil", value: "317" },
         { label: "Orissa → Odisha", value: "96th Amendment (23rd Sep 2011)" },
         { label: "Oriya → Odia", value: "97th Amendment (23rd Sep 2011)" },
         { label: "Area Rank in India", value: "8th" },
+        { label: "In 1936 Odisha have", value: "6 districts" },
+        { label: "In 1949 Odisha have", value: "13 districts" },
+        { label: "In 1993 Odisha have", value: "30 districts" },
+        { label: "Smallest District", value: "Jagatsinghpur" },
+        { label: "Largest District", value: "Mayurbhanj" },
+        { label: "Odisha have 3 divisions", value: "Central(Cuttack), South(Berhampur), North(Sambalpur)" },
+        { label: "Bihar & odisha get Separated from Bengal Residency", value: "22nd Mar 1912" },
+
       ],
     },
 
     {
-      title: "Government",
-      color: "#f8d7da", // light red
+      title: "Boundary",
+      color: "#dffbff", // aqua
       items: [
-        { label: "Governor", value: "Ganeshi Lal" },
-        { label: "Chief Minister", value: "Naveen Patnaik" },
-        { label: "Capital", value: "Bhubaneswar" },
+        {
+          label: "Total Boundaries",
+          value: "4 (West Bengal, Jharkhand, Chhattisgarh, Andhra Pradesh)"
+        },
+        {
+          label: "West Bengal Border (2)",
+          value: "Balasore, Mayurbhanj"
+        },
+        {
+          label: "Jharkhand Border (3)",
+          value: "Mayurbhanj, Keonjhar, Sundargarh"
+        },
+        {
+          label: "Chhattisgarh Border (8)",
+          value: "Sundargargh, Jharsuguda, Bargarh, Nuapada, Kalahandi, Nabarangpur, Koraput, Malkangiri "
+        },
+        {
+          label: "Andhra Pradesh Border (5)",
+          value: "Malkangiri, Koraput, Rayagada, Gajapati, Ganjam "
+        },
+      ],
+    },
+
+        {
+      title: "Symbols Of Odisha",
+      color: "#ff9fb7ff", // aqua
+      items: [
+        {
+          label: "State Animal",
+          value: "Sambar(Deer)"
+        },
+        {
+          label: "State Bird",
+          value: "Indian Roller (ଭଦଭଦଳିଆ)"
+        },
+        {
+          label: "State Flower",
+          value: "Ashoka"
+        },
+        {
+          label: "State Tree",
+          value: "Ashwathha"
+          
+        },
+          {
+          label: "State Fish",
+          value: "Mahanadi mahseer"
+        },
+          {
+          label: "State Song",
+          value: "Bande Utkala Janani (2020) Kantakabi Laxmikanta Mohapatra"
+        },
+
+      ],
+    },
+    {
+      title: "Population (2011 Census)",
+      color: "#ffcd9fff", // aqua
+      items: [
+        {
+          label: "Total Population",
+          value: "41,974,218"
+        },
+        {
+          label: "Population Wise Rank",
+          value: "11th"
+        },
+        {
+          label: "Highest Population",
+          value: "Ganjam"
+        },
+        {
+          label: "Lowest Population",
+          value: "Deogarh"
+        },
+        {
+          label: "Male Population",
+          value: "21,212,136"
+        },
+        {
+          label: "Female Population",
+          value: "20,762,082"
+        },
+        {
+          label: "Population Growth",
+          value: "13.97%"
+        },
+        {
+          label: "Dense Population",
+          value: "Khorda"
+        },
+        {
+          label: "Highest Sex Ratio",
+          value: "Khorda"
+        },
+        {
+          label: "Lowest Sex Ratio",
+          value: "Nayagarh"
+
+        },
+        {
+          label: "According to 2011C ST Population Percentage in Odisha",
+          value: "32"
+
+        },
       ],
     },
 
     {
-      title: "Demographics",
-      color: "#d1ecf1", // light blue
+      title: "Language",
+      color: "#ff9fb7ff", // aqua
       items: [
-        { label: "Population", value: "4.2 Crores (approx.)" },
-        { label: "Official Language", value: "Odia" },
-        { label: "Area", value: "155,707 km²" },
+        {
+          label: "Official Language",
+          value: "Odia (84%)"
+        },
+        {
+          label: "Total Official Language",
+          value: "22"
+        },
+        {
+          label: "Total Official Language",
+          value: "11"
+        },
+        {
+          label: "Odia is also Classical language on ",
+          value: "11th March 2014"
+        },
+
       ],
     },
 
-    {
-      title: "State Symbols",
-      color: "#d4edda", // light green
-      items: [
-        { label: "State Bird", value: "Indian Roller (Neelkanth)" },
-        { label: "State Animal", value: "Sambar Deer" },
-        { label: "State Tree", value: "Sal Tree" },
-        { label: "State Flower", value: "Ashoka Flower" },
-      ],
-    },
-
-    {
-      title: "Culture & Tourism",
-      color: "#fff3cd", // light yellow
-      items: [
-        { label: "Famous Temple", value: "Jagannath Temple, Puri" },
-        { label: "Famous Dance", value: "Odissi" },
-        { label: "Famous Cuisine", value: "Pakhala, Rasgulla" },
-      ],
-    },
-  ],
+  ]
 };
 
 export default odishaData;
